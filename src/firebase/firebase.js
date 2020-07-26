d4071f2795352dc53c2597472d6a41b5fc66c834
EldhosAji <eldhosaji1998@gmail.com>
2020-07-26 12:11:57 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,15 @@
 import firebase from "firebase";
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDr-5G20-nf99u3Hc08Pp4BmFoVLGScnu8",
-  authDomain: "kudos-6539b.firebaseapp.com",
-  databaseURL: "https://kudos-6539b.firebaseio.com",
-  projectId: "kudos-6539b",
-  storageBucket: "kudos-6539b.appspot.com",
-  messagingSenderId: "635822426812",
-  appId: "1:635822426812:web:ac386ff0ea1b7907a3deb9",
-  measurementId: "G-Z6FCN954XR",
+  //Firebase config
+  
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "=",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 });
 
 const db = firebaseApp.firestore();
